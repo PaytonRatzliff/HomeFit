31b0afa34f08da7e96b5c5b493a6dd8b02ed8be0
PaytonRatzliff <paytor@uw.edu>
2017-02-20 16:10:49 -0800

    updated task page again

--- taskScript.js
+++ taskScript.js
@@ -1,8 +1,11 @@
 window.onload = function() {
-	document.getElementById("clickable").onclick = function(){loadTasks()};;	
+	document.getElementById("clickable").onclick = function(){loadTasks()};
+	document.getElementById("close").onclick = function(){closeTasks()};
 };
 
-
+function closeTasks() {
+	document.getElementById("tasks").style.display = "none";
+}
 
 function loadTasks() {
 	document.getElementById("tasks").style.display = "block";
